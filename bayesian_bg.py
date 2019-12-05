326ef8b3646cd7bd5c18da3c6dee5b99e9dca594
aditikrishak <40263663+aditikrishak@users.noreply.github.com>
2019-12-05 20:15:18 +0530

    Update bayesian_bg.py

--- bayesian_bg.py
+++ bayesian_bg.py
@@ -1,4 +1,6 @@
-# Bayesian Analysis : Background only
+
+# this code calculates evidence for background only hypothesis
+ 
 
 import numpy as np 
 from scipy import optimize , stats
@@ -8,70 +10,46 @@ import time
 from dynesty import NestedSampler
 from multiprocessing import Pool
 from contextlib import closing
-import matplotlib.pyplot as plt
-plt.style.use('ggplot')
 
 
-f=input('enter path to data file : ')
-data1 = np.loadtxt(f,delimiter=',')                    
 
-g=input('enter path to data file : ')
-data2 = np.loadtxt(g,delimiter=',')                    
+#f=os.path.expanduser("~")+"/Desktop/COSINE100/data/c2_data.txt"
+data1 = np.loadtxt("c2_data.txt",delimiter=',')                    
 
-h=input('enter path to data file : ')
-data3 = np.loadtxt(h,delimiter=',')                    
+#g=os.path.expanduser("~")+"/Desktop/COSINE100/data/c3_data.txt"
+data2 = np.loadtxt("c3_data.txt",delimiter=',')                    
 
-k=input('enter path to data file : ')
-data4 = np.loadtxt(k,delimiter=',')                    
+#h=os.path.expanduser("~")+"/Desktop/COSINE100/data/c4_data.txt"
+data3 = np.loadtxt("c4_data.txt",delimiter=',')                    
 
-l=input('enter path to data file : ')
-data5 = np.loadtxt(l,delimiter=',')                    
+#k=os.path.expanduser("~")+"/Desktop/COSINE100/data/c6_data.txt"
+data4 = np.loadtxt("c6_data.txt",delimiter=',')                    
+
+#l=os.path.expanduser("~")+"/Desktop/COSINE100/data/c7_data.txt"
+data5 = np.loadtxt("c7_data.txt",delimiter=',')                    
 
 data=np.hstack((data1,data2,data3,data4,data5))
-
 tol=0.1
 
 def fit_bg(x,c,p0,p1):
     	return c + p0*np.exp(-np.log(2)*x/p1)
 
-def chi2_bg(P):
-    
-        sigma1=[ np.sqrt( (data1[3,i]**2) + ((-P[1]*np.log(2)*np.exp(-np.log(2)*data1[0,i]/P[2])/P[2])*data1[2,i])**2 ) for i in range(len(data1[0])) ]
-        y_fit1=fit_bg(data1[0],P[0],P[1],P[2])
-        
-        sigma2=[ np.sqrt( (data2[3,i]**2) + ((-P[4]*np.log(2)*np.exp(-np.log(2)*data2[0,i]/P[5])/P[5])*data2[2,i])**2 ) for i in range(len(data2[0])) ]
-        y_fit2=fit_bg(data2[0],P[3],P[4],P[5])
-        
-        sigma3=[ np.sqrt( (data3[3,i]**2) + ((-P[7]*np.log(2)*np.exp(-np.log(2)*data3[0,i]/P[8])/P[8])*data3[2,i])**2 ) for i in range(len(data3[0])) ]
-        y_fit3=fit_bg(data3[0],P[6],P[7],P[8])
-        
-        sigma4=[ np.sqrt( (data4[3,i]**2) + ((-P[10]*np.log(2)*np.exp(-np.log(2)*data4[0,i]/P[11])/P[11])*data4[2,i])**2 ) for i in range(len(data4[0])) ]
-        y_fit4=fit_bg(data4[0],P[9],P[10],P[11])
-        
-        sigma5=[ np.sqrt( (data5[3,i]**2) + ((-P[13]*np.log(2)*np.exp(-np.log(2)*data5[0,i]/P[14])/P[14])*data5[2,i])**2 ) for i in range(len(data5[0])) ]
-        y_fit5=fit_bg(data5[0],P[12],P[13],P[14])    
-        
-        y_fit=np.hstack((y_fit1,y_fit2,y_fit3,y_fit4,y_fit5))
-        sigma=np.hstack((sigma1,sigma2,sigma3,sigma4,sigma5))        
-
-        r=(data[1]-y_fit)/sigma
-        return np.sum(r**2)
 
 def log_likelihood_bg(P):
     
-        sigma1=[ np.sqrt( (data1[3,i]**2) + ((-P[1]*np.log(2)*np.exp(-np.log(2)*data1[0,i]/P[2])/P[2])*data1[2,i])**2 ) for i in range(len(data1[0])) ]
+        sigma1=[ data1[3,i] for i in range(len(data1[0])) ]
         y_fit1=fit_bg(data1[0],P[0],P[1],P[2])
         
-        sigma2=[ np.sqrt( (data2[3,i]**2) + ((-P[4]*np.log(2)*np.exp(-np.log(2)*data2[0,i]/P[5])/P[5])*data2[2,i])**2 ) for i in range(len(data2[0])) ]
+        sigma2=[  data2[3,i]  for i in range(len(data2[0])) ]
         y_fit2=fit_bg(data2[0],P[3],P[4],P[5])
         
-        sigma3=[ np.sqrt( (data3[3,i]**2) + ((-P[7]*np.log(2)*np.exp(-np.log(2)*data3[0,i]/P[8])/P[8])*data3[2,i])**2 ) for i in range(len(data3[0])) ]
+        sigma3=[ data3[3,i]  for i in range(len(data3[0])) ]
         y_fit3=fit_bg(data3[0],P[6],P[7],P[8])
         
-        sigma4=[ np.sqrt( (data4[3,i]**2) + ((-P[10]*np.log(2)*np.exp(-np.log(2)*data4[0,i]/P[11])/P[11])*data4[2,i])**2 ) for i in range(len(data4[0])) ]
+        sigma4=[  data4[3,i]  for i in range(len(data4[0])) ]
         y_fit4=fit_bg(data4[0],P[9],P[10],P[11])
         
-        sigma5=[ np.sqrt( (data5[3,i]**2) + ((-P[13]*np.log(2)*np.exp(-np.log(2)*data5[0,i]/P[14])/P[14])*data5[2,i])**2 ) for i in range(len(data5[0])) ]
+        sigma5=[ data5[3,i]  for i in range(len(data5[0])) ]
         y_fit5=fit_bg(data5[0],P[12],P[13],P[14])    
 
         y_fit=np.hstack((y_fit1,y_fit2,y_fit3,y_fit4,y_fit5))
@@ -83,10 +61,9 @@ def log_likelihood_bg(P):
 a1=100.0*np.max(data[1])
 b1=30000.0
 
-    
 
 def prior_transform_bgnoprior(P):
-        return np.array([(a1+100)*P[0]-100,a1*P[1],b1*P[2],P[3]*(a1+100)-100,a1*P[4],P[5]*b1,(a1+100)*P[6]-100,P[7]*a1,P[8]*b1,P[9]*(a1+100)-100,P[10]*a1,P[11]*b1,P[12]*(a1+100)-100,P[13]*a1,P[14]*b1])
+        return np.array([a1*P[0],a1*P[1],b1*P[2],a1*P[3],a1*P[4],P[5]*b1,a1*P[6],P[7]*a1,P[8]*b1,P[9]*a1,P[10]*a1,P[11]*b1,P[12]*a1,P[13]*a1,P[14]*b1])
 
 
 
@@ -104,10 +81,9 @@ def nestle_multi_bg():
         print(res.summary())
         return res.logz[-1],res.logzerr[-1]
 
+#----------------------------------------------------------------------------
 
-
-print ("using no priors on background model: a1 changed to 10000. priors starting from negative values for C. p0 & p1 been positive. bound=balls")
+print ("Evidence for Background")
 Z1,Z1err = nestle_multi_bg()
 print (Z1)
 print (Z1err)
-
